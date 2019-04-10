3130be6564fb87c75243ab0a3a3bb79295ebe14a
刘锦洋 <779542115@qq.com>
2019-04-10 09:32:02 +0800

    test11

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -51,7 +51,7 @@ module.exports = {
       }
     ],
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: "https://github.com/b779542115/test",
+    repo: "https://github.com/b779542115/Blog",
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
     repoLabel: "查看源码",
